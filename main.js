b356cacc5c708e2e9feb93bb6c519568fd2dc389
Sergi Juanola <korcholis@gmail.com>
2014-11-27 01:14:18 +0100

    Devices detected, install prepared

--- main.js
+++ main.js
@@ -3,10 +3,12 @@ var exec = require('child_process').exec;
 
 module.exports = {
   init : function() {
-    exec("adb help", function(error, stdout, stderr) {
+    exec("adb start-server", function(error, stdout, stderr) {
       if (error) {
         console.error("You need to first add adb and android commands to the path.");
         process.exit();
+      } else {
+        console.log(stdout);
       }
     });
   },
@@ -19,37 +21,74 @@ module.exports = {
   compile : function(path, flags) {
 
   },
-  install : function(apk, devices) {
-    devices = this._devices_from_var(devices);
-
-    for (var i = 0; i < devices.length; i++) {
-      devices[i]
-    }
+  install : function(apk, devices_to_use) {
+    this._devices_from_var(devices_to_use, function(devices) {
+      for (var i = 0; i < devices.length; i++) {
+        console.log('Installing apk ' + apk + ' in device ' + devices[i]);
+        exec("adb install -rtd " + apk + " -s " + devices[i]);
+      }
+    });
   },
-  get_main_device : function() {
-
+  get_main_device : function(callback) {
+    exec("adb devices", function(error, stdout, stderr) {
+      var findDeviceId = /\n([\d\w\.\:]+)\s+/m;
+      var match = findDeviceId.exec(stdout);
+      if (match != null) {
+        callback([match[1]]);
+      } else {
+        callback([]);
+      }
+    });
   },
-  get_all_devices : function() {
-
+  get_all_devices : function(callback) {
+    exec("adb devices", function(error, stdout, stderr) {
+      var findDeviceId = /\n([\d\w\.\:]+)\s+/m;
+      var match = findDeviceId.exec(stdout);
+      if (match != null) {
+        match.shift();
+        callback(match);
+      } else {
+        callback([]);
+      }
+    });
   },
-  logcat : function(devices) {
-    devices = this._devices_from_var(devices);
+  logcat : function(devices_to_use) {
+    this._devices_from_var(devices_to_use, function(devices) {
+      for (var i = 0; i < devices.length; i++) {
+        console.log('Logging device ' + devices[i]);
+      }
+    });
   },
-  _devices_from_var : function(devices) {
-    if ("undefined" === typeof devices) {
-      devices = [this.get_main_device()];
-    } else if (Array === devices.constructor) {
-      // check all of them are available
-    } else if ("string" === typeof devices) {
-      if ("first" === devices || "1" == devices || "main" == devices) {
-        devices = [this.get_main_device()];
-      } else if ("all" === devices || "*" === devices) {
-        devices = this.get_all_devices();
+  _devices_from_var : function(devices_to_use, callback) {
+    if ("undefined" === typeof devices_to_use) {
+      this.get_main_device(function(device_found) {
+        callback(device_found);
+      });
+    } else if ("string" === typeof devices_to_use) {
+      if ("first" === devices_to_use || "1" == devices_to_use || "main" == devices_to_use) {
+        this.get_main_device(function(device_found) {
+          callback(device_found);
+        });
+      } else if ("all" === devices_to_use || "*" === devices_to_use) {
+        this.get_all_devices(function(devices_found) {
+          callback(devices_found);
+        });
       } else {
         console.error("devices contains no valid items. It should include a list of device IDs, or any of the wildcards first, 1, main, all or *");
         process.exit();
       }
+    } else if (Array === devices_to_use.constructor) {
+      this.get_all_devices(function(devices_found) {
+
+        for(var i = devices_to_use.length-1; i >= 0; i--) {
+          if(devices_found.indexOf(devices_to_use[i]) == -1) {
+            devices_to_use.splice(i, 1);
+          }
+        }
+        callback(devices_to_use);
+      });
     }
-    return devices;
   }
 }
+
+module.exports.init();
