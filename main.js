1156633dd173114c4bbdd624b23914529c618099
Sergi Juanola <it6@languagecourse.net>
2014-12-03 16:25:04 +0100

    Ant compilation done

--- main.js
+++ main.js
@@ -22,6 +22,8 @@
 var sys = require('sys');
 var exec = require('child_process').exec;
 var spawn = require('child_process').spawn;
+var path_utils = require('path');
+var fs = require('fs');
 
 module.exports = {
 
@@ -42,12 +44,11 @@ module.exports = {
    *
    * Here you also have a chance to set any configuration for the module.
    *
-   * @param {object} config - Include a config to this module.
-   *                          The default for this config is { gentle : true }.
-   *                          The currently available parameters to config are:
-   *                            - {boolean} gentle. Exit the process on error if
-   *                              false, or pass the error to the callback on
-   *                              true
+   * @param {object} config - Include a config to this module. The currently
+   *                          available parameters to config are:
+   *                          <ul><li>{boolean} gentle. Exit the process on
+   *                          error if false, or pass the error to the callback
+   *                          on true. Defaults to true.</li></ul>
    * @param {callback} callback - Pass on a callback to know when the adb server
    *                              is started, or if it had an error. It takes
    *                              one parameter, containing an {Error} error or
@@ -66,7 +67,7 @@ module.exports = {
   },
 
   /**
-   * Call this function if you want to clear any old reference to spawned
+   * Call this function if you want to clear any old reference to spawned logcat
    * children that may end up consumig a lot of memory.
    */
   reset : function() {
@@ -77,7 +78,7 @@ module.exports = {
   },
 
   create : function(app_name, package_name, path,
-                    min_version, compile_version, options) {
+                    min_version, compile_version, flags) {
 
   },
 
@@ -85,8 +86,48 @@ module.exports = {
 
   },
 
-  compile : function(path, flags) {
+  /**
+   * Take a project and compile it with some specific configurations
+   *
+   * @param {string} path - The root directory of the project
+   * @param {callback} callback - The callback for when a compilation is done.
+   *                              It takes one parameter, {string} apk, which
+   *                              contains the path to the compiled apk, or null
+   *                              if it wasn't found.
+   * @param {object} flags - Optional list of flags to determine what to do in
+   *                         this compilation. The params are:
+   *                         - {string} project_type. What kind of project we
+   *                           have to compile. Options are 'gradle' and 'ant'.
+   *                           There's also 'eclipse', which is an alias of
+   *                           'ant'. The default is 'gradle'.
+   *                         - {string} compile_type. Any of the possible values
+   *                           available to compile to. It depends on what
+   *                           project_type you selected. For command line
+   *                           projects, these are:
+   *                           - 'debug'. Creates a debug package. It enables
+   *                             you to test it.
+   *                           - 'release'. Create a release package. It's as an
+   *                             apk should work when installed on a device.
+   *                           - 'instrument'. Create an instrumented debug
+   *                             package.
+   *                         The default value is 'debug'. More info from ant
+   *                         here: http://developer.android.com/tools/building/building-cmdline.html
+   *                         - {boolean} clean_first. Set this to true to clean.
+   *                           The project before compiling. This ensures there
+   *                           are no old resources left. Defaults to false.
+   */
+  compile : function(path, callback, flags) {
+    flags = flags || {
+      project_type : 'gradle',
+      compile_type : 'debug',
+      clean_first : false
+    };
 
+    if ('gradle' == flags.project_type) {
+      this._compile_gradle(path, callback, flags);
+    } else if(['ant', 'eclipse'].indexOf(flags.project_type) !== -1) {
+      this._compile_ant(path, callback, flags);
+    }
   },
 
   install : function(apk, devices_to_use) {
@@ -304,7 +345,7 @@ module.exports = {
    * Take a logcat long data row and convert it into a JSON. This is useful to
    * avoid having to parse it manually.
    * @param {string} data - A line of logcat data
-   * @return {object} - A parsed JSON object, or null if not parseable.
+   * @return {object} A parsed JSON object, or null if not parseable.
    */
   _convert_to_json : function(data) {
     data = "" + data;
@@ -321,5 +362,60 @@ module.exports = {
       tag : results[6],
       message : results[7]
     };
+  },
+
+  _compile_gradle : function(path, callback, flags) {
+
+  },
+
+  /**
+   * Compile a project based on the ant system. The params are the same as
+   * <code>compile</code
+   */
+  _compile_ant : function(path, callback, flags) {
+    var android = this;
+    if (['debug', 'release', 'instrument'].indexOf(flags.compile_type) !== -1) {
+      if (true == flags.clean_first) {
+        exec("ant clean", { cwd : path }, function(error, stdout, stderr) {
+          if (error instanceof Error) {
+            throw error;
+          }
+          exec("ant " + flags.compile_type, { cwd : path }, function(error, stdout, stderr) {
+            if (error instanceof Error) {
+              throw error;
+            }
+            callback(android._return_apk(path));
+          });
+        });
+      } else {
+        exec("ant " + flags.compile_type, { cwd : path }, function(error, stdout, stderr) {
+          if (error instanceof Error) {
+            throw error;
+          }
+          callback(android._return_apk(path));
+        });
+      }
+    }
+  },
+
+  /**
+   * Get the apk filename and path for the current project.
+   *
+   * @param {string} path - The root directory of the project
+   *
+   * @return {string} The full path and name to the apk file, or null if not
+   *                  found.
+   *
+   */
+  _return_apk : function(path) {
+    var bin_path = path_utils.join(path, 'bin');
+    var files = fs.readdirSync(bin_path);
+    var regex = /^(?:(?!\-unaligned).)*\.apk$/gm;
+    for (var i = 0; i < files.length; i++) {
+      if (regex.test(files[i])) {
+        return path_utils.join(bin_path, files[i]);
+      }
+    }
+    return null;
   }
 }
